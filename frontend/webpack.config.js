57ac86da60252de47741aad45dd3878dfbd873c4
zairong <justiceman3211@gmail.com>
2025-09-09 14:42:50 +0800

    更新 webpack.config.js 檔案，將 publicPath 設定從 '/' 修改為 './'，以改善資源加載路徑的相對性。

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
-    publicPath: '/'
+    publicPath: './'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
