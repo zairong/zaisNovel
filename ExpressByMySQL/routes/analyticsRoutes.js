ca611af18d50d439a1c1ff0020298337788f688f
zairong <justiceman3211@gmail.com>
2025-09-10 14:14:45 +0800

    新增可選認證中間件，調整年齡分布與可用年份的路由，避免不必要的審計，提升 API 效能。

--- ExpressByMySQL/routes/analyticsRoutes.js
+++ ExpressByMySQL/routes/analyticsRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { authenticate } = require('../middleware')
+const { authenticate, optionalAuth } = require('../middleware')
 const { analyticsController: ctrl } = require('../controllers')
-const { wrap } = require('../utils/http')
+const { wrap, wrapNoAudit } = require('../utils/http')
 
 router.get('/views', ...wrap(authenticate)(ctrl.viewsHistory))
 router.get('/downloads', ...wrap(authenticate)(ctrl.downloadsHistory))
 router.get('/favorites', ...wrap(authenticate)(ctrl.favoritesHistory))
 router.get('/my-books', ...wrap(authenticate)(ctrl.myBooks))
-router.get('/age-distribution', ...wrap(authenticate)(ctrl.getAgeDistribution))
-router.get('/available-years', ...wrap(authenticate)(ctrl.getAvailableYears))
+router.get('/age-distribution', ...wrapNoAudit(optionalAuth)(ctrl.getAgeDistribution))
+router.get('/available-years', ...wrapNoAudit(optionalAuth)(ctrl.getAvailableYears))
 
 module.exports = router
 
